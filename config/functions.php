6e70ae37fce48cbde3553cc87df19cef24b8b61b
unknown <business.urfavteddybear@gmail.com>
2024-06-21 23:58:55 +0800

    feat: added edit user feature

--- config/functions.php
+++ config/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function uploadimg(){
+function uploadimg($url = null){
     $namafile   = $_FILES['image']['name'];
     $ukuran     = $_FILES['image']['size'];
     $tmp        = $_FILES['image']['tmp_name'];
@@ -10,21 +10,38 @@ function uploadimg(){
     $ekstensiGambar         = explode('.', $namafile);
     $ekstensiGambar         = strtolower(end($ekstensiGambar));
     if(!in_array($ekstensiGambar, $ekstensiGambarValid)) {
+        if ($url != null) {
+            echo '<script>
+            alert("File yg anda upload bukan gambar, data gagal diupload ! ");
+            document.location.href = "' . $url . '";
+            </script>';
+            die();
+        } else{
+
         echo '<script>
                 alert("File yg anda upload bukan gambar, data gagal ditambahkan ! ");
         
             </script>';
             return false;
+        }
     }
 
 
     // validasi ukuran gambar max 1 MB
     if ($ukuran > 1000000) {
+        if ($url != null) {
+            echo '<script>
+            alert("Ukuran gambar melebihi 1MB, data gagal diupdate! ");
+            document.location.href = "' . $url . '";
+            </script>';
+            die();
+        } else{
         echo '<script>
                 alert("Ukuran gambar tidak boleh lebih dari 1 MB ");
         
             </script>';
             return false;
+        }
     }
 
     $namaFileBaru   = rand(10, 1000) . '-' . $namafile;
--- module/mode-user.php
+++ module/mode-user.php
@@ -56,6 +56,82 @@ function delete($id, $foto){
         unlink('../asset/image/' . $foto);
     }
 
+    return mysqli_affected_rows($conn);   
+}
+
+
+function selectUser1($level){
+    $result = null;
+    if ($level == 1) {
+        $result = "selected";
+    }
+    return $result;
+}
+
+function selectUser2($level){
+    $result = null;
+    if ($level == 2) {
+        $result = "selected";
+    }
+    return $result;
+}
+
+function selectUser3($level){
+    $result = null;
+    if ($level == 3) {
+        $result = "selected";
+    }
+    return $result;
+}
+
+function update($data){
+    global $conn;
+    $iduser     = mysqli_real_escape_string($conn, $data['id']);
+    $username   = strtolower(mysqli_real_escape_string($conn, $data['username']));
+    $fullname   = mysqli_real_escape_string($conn, $data['fullname']);
+    $level   = mysqli_real_escape_string($conn, $data['level']);
+    $address   = mysqli_real_escape_string($conn, $data['address']);
+    $gambar   = mysqli_real_escape_string($conn, $_FILES['image']['name']);
+    $fotoLama   = mysqli_real_escape_string($conn, $data['oldImg']);
+
+    // cek username sekarang
+    $queryUsername = mysqli_query($conn, "SELECT * FROM tbl_user WHERE userid = $iduser");
+    $dataUsername   = mysqli_fetch_assoc($queryUsername);
+    $curUsername    = $dataUsername['username'];
+
+    // cek username baru
+    $newUsername    = mysqli_query($conn, "SELECT username FROM tbl_user WHERE username = '$username'");
+
+    if ($username !== $curUsername) {
+        if (mysqli_num_rows($newUsername)) {
+            echo '<script>
+                alert("Username sudah terpakai, user gagal diedit");
+                document.location.href = "data-user.php";
+            </script>';
+            return false;
+        }
+    }
+
+    // cek gambar
+    if ($gambar != null) {
+        $url        = "data-user.php";
+        $imgUser    = uploadimg($url);
+        if ($fotoLama != 'default.png') {
+            @unlink('../asset/image' . $fotoLama);
+        }
+    } else{
+        $imgUser = $fotoLama;
+    }
+
+    mysqli_query($conn, "UPDATE tbl_user SET
+                                        username    = '$username',    
+                                        fullname    = '$fullname',    
+                                        address    = '$address', 
+                                        level    = '$level', 
+                                        foto    = '$imgUser'
+                                        WHERE userid = $iduser 
+                                            ");
+   
     return mysqli_affected_rows($conn);
 }
 ?>
