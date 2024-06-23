f65d36d44bba35c3717861b4ebc9c3068e2feac7
unknown <business.urfavteddybear@gmail.com>
2024-06-23 11:49:50 +0800

    feat: change password

--- error-page.php
+++ error-page.php
@@ -50,7 +50,7 @@ require "template/sidebar.php";
 
           <p>
             We could not find the page you were looking for.
-            Meanwhile, you may <a href="dashboard.php">return to dashboard</a> or try using the search form.
+            Meanwhile, you may <a href="dashboard.php">return to dashboard</a>
           </p>
 
         </div>
@@ -59,30 +59,6 @@ require "template/sidebar.php";
       <!-- /.error-page -->
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
-</body>
-</html>
+     <?php
+      require "template/footer.php";
+     ?>
