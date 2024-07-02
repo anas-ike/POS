c3eddef5d80d7113c653fb7986626133a8757cb0
unknown <business.urfavteddybear@gmail.com>
2024-07-03 00:04:08 +0800

    style: removing splash screen

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loading..</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="loading">
-        <div class="title">Loading..</div>
-        <div class="progress-bar">
-            <div class="progress"></div>
-        </div>
-    </div>
+<?php
 
-    <script src="script.js"></script>
-</body>
-</html>
+header('location: auth/login.php');
+
+?>
